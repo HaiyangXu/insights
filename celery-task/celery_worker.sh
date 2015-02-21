232527d9b15950bd8a96d4bac625b3ccb2c1868c
haiyangxu <findway.xu@gmail.com>
2015-02-21 06:32:21 +0000

    celery fix

--- celery-task/celery_worker.sh
+++ celery-task/celery_worker.sh
@@ -1,3 +1,4 @@
 #celery  worker -A tasks  --loglevel=info # sechedule
+source env/bin/active
 celery  worker -A celery-task.tasks -B --detach --loglevel=info # sechedule and worker
 #celery  worker  --detach -A tasks --loglevel=info # daemon
