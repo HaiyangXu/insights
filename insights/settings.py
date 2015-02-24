5927350f151dfcc007bed8ed7b2d401d4e62e2d7
haiyangxu <findway.xu@gmail.com>
2015-02-24 16:26:26 +0000

    commit with fabric

--- insights/settings.py
+++ insights/settings.py
@@ -11,8 +11,8 @@ try:
     os.mkdir(dbdir)
 except Exception, e:
     pass
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dbdir, 'data.db')
-SQLALCHEMY_DATABASE_URI ='postgresql://dbfqwmlqiopsfo:OpXfSaGk3mqoqbLz9HRRI9DX0z@ec2-54-204-45-65.compute-1.amazonaws.com:5432/db07o0s1t78g8i'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dbdir, 'data.db')
+#SQLALCHEMY_DATABASE_URI ='postgresql://dbfqwmlqiopsfo:OpXfSaGk3mqoqbLz9HRRI9DX0z@ec2-54-204-45-65.compute-1.amazonaws.com:5432/db07o0s1t78g8i'
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(dbdir, 'db_repository')
 
