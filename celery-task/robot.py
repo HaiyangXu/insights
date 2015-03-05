8c7e322dad69712b51aec45eedd3e73eae030293
haiyangxu <findway.xu@gmail.com>
2015-03-05 14:22:17 +0000

    minor Refactoring

--- celery-task/robot.py
+++ celery-task/robot.py
@@ -2,6 +2,8 @@
 import requests
 import feedparser
 import re
+from datetime import datetime
+from insights import models,db
 
 description_size=200
 
@@ -14,18 +16,33 @@ def striphtml(data):
     else:
         return plain[:description_size]
     
-def grabrss(rss_url):
-    result = feedparser.parse(rss_url)
-    return [
-        {'title':item.title,'link':item.link ,'des':striphtml(item.description)}
-        for item in result.entries 
-        ]
+def grabrss(rss_url,feed_id):
+    feed_data = feedparser.parse(rss_url)
+    channel,items =feed_data.feed,feed_data.entries
+    count=0
+    for item in items:
+        feeditem = models.FeedsItem(
+            title=item.title,
+            link=item.link,
+            des=striphtml(item.description),
+            feed_id=feed_id,
+            date=datetime.now()
+            )
+        db.session.add(feeditem)
+        try:
+            db.session.commit()
+            count=count+1
+        except Exception, e:
+            db.session.rollback()
+            
+    return count
+            
+   
 
 
+def getFeeds():
+    return models.Feeds.query.all()
 
 
 if __name__=='__main__':
-    res=grabrss('http://mindhacks.cn/feed/')
-    for it in res:
-        print it['title']
-        print it['link']
+    pass
--- celery-task/tasks.py
+++ celery-task/tasks.py
@@ -1,9 +1,9 @@
 from celery import Celery
 import sys
 sys.path.append('..')
-from insights import models,db
+
 import robot
-from datetime import datetime
+
 celery = Celery(__name__)
 celery.config_from_object('settings')
 #celery = Celery('tasks', broker='amqp://guest@localhost//')
@@ -12,24 +12,13 @@ celery.config_from_object('settings')
 
 @celery.task
 def grabrss(rss_url,feed_id):
-    res= robot.grabrss(rss_url)
-    for item in res:
-        title=item['title']
-        link=item['link']
-        #feed_id=item['feed_id']
-        des=item['des']
-        feeditem = models.FeedsItem(title=title,link=link,des=des,feed_id=feed_id,date=datetime.now())
-        db.session.add(feeditem)
-    try:
-        db.session.commit()
-    except Exception, e:
-        db.session.rollback()
+    return robot.grabrss(rss_url,feed_id)
     
 @celery.task
 def addToQueue():
-    for feed in models.Feeds.query.all():
-        if feed.rss :
-            grabrss.delay(feed.rss,feed.id)
+    feeds=robot.getFeeds()
+    for feed in feeds:
+        grabrss.delay(feed.rss,feed.id)
 
 
     
