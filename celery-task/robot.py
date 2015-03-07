f6b876720bde642a41db897b27e0d0e71edf05e0
haiyangxu <findway.xu@gmail.com>
2015-03-07 14:48:42 +0000

    grab rss entry time

--- celery-task/robot.py
+++ celery-task/robot.py
@@ -5,7 +5,7 @@ import re
 from datetime import datetime
 from insights import models,db
 from HTMLParser import HTMLParser
-
+from time import mktime
 description_size=200
 
 def striphtml(data):
@@ -22,13 +22,23 @@ def grabrss(rss_url,feed_id):
     channel,items =feed_data.feed,feed_data.entries
     count=0
     html_parser = HTMLParser()
+    def gettime(parsed):
+        return datetime.fromtimestamp(mktime(parsed))
     for item in items:
+        global date
+        if hasattr(item,'published_parsed'):
+            date=gettime(item.published_parsed)
+        elif hasattr(item,'updated_parsed'):
+            date=gettime(item.updated_parsed)
+        elif hasattr(item,'created_parsed'):
+            date=gettime(item.created_parsed)
+        
         feeditem = models.FeedsItem(
             title=html_parser.unescape(item.title),
             link=item.link,
             des=html_parser.unescape(striphtml(item.description)),
             feed_id=feed_id,
-            date=datetime.now()
+            date=date
             )
         db.session.add(feeditem)
         try:
